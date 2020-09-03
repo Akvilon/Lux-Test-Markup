a70b561a9705d368a6559aab675a7dc9f4f25281
Eugen Pavlov <Eugen.Pavlov@gmail.com>
2020-09-03 17:12:53 +0300

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,27 @@
 
+let burderBtn = document.getElementById('burgerBtn');
+let burgerMenu = document.getElementById('burgerMenu');
+let blackout = document.getElementById('blackout');
+let closeMenu = document.getElementById('closeMenu');
+
+burderBtn.addEventListener('click', () => {
+  blackout.classList.add('open');
+  burgerMenu.classList.add('active')
+})
+
+blackout.addEventListener('click', () => {
+  blackout.classList.remove('open');
+  burgerMenu.classList.remove('active')
+})
+
+closeMenu.addEventListener('click', () => {
+  blackout.classList.remove('open');
+  burgerMenu.classList.remove('active')
+})
+
+
+
+
 // ACCORDION
 let acc = document.getElementsByClassName("accordion");
 let i;
